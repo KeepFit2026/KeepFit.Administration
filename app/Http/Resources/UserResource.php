da2b8f7f8908cc35745d8273abd729c0b4daf25f
EstebanSmolak19 <estebansmolak@gmail.com>
2026-04-19 16:06:19 +0200

    fix: nv champs UserRessource

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -15,9 +15,10 @@ class UserResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'    => $this->id,
-            'name'  => $this->user->name ?? 'Inconnu',
-            'email' => $this->email,
+            'id'                   => $this->id,
+            'name'                 => $this->user->name ?? 'Inconnu',
+            'email'                => $this->email,
+            'onboarding_completed' => $this->onboarding_completed
         ];
     }
 }
