6e21005bc43baf3c144016e772c795bdd4372f99
EstebanSmolak19 <estebansmolak@gmail.com>
2026-04-27 15:40:17 +0200

    feat: Passage niveau suivant

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -3,25 +3,40 @@
 namespace App\Services;
 
 use App\Http\Resources\UserResource;
+use App\Models\Level;
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Log;
 
 class UserService
 {
-    public function getCurrentUser()
+    public function getCurrentUser(): UserResource
     {
-        Log::info(Auth::user());
         return new UserResource(Auth::user());
     }
 
-    public function addXp(int $amount)
+    public function addXp(int $amount): void
     {
         $profile = User::where('account_id', Auth::id())->first();
 
         if ($profile) {
+            $level = Level::where('number', $profile->current_level + 1)->first(); // Trouve le niveau de l'utilisateur
             $profile->current_xp += $amount;
+
+            $this->nextLevel($level, $profile);
+
             $profile->save();
+        }
+    }
+
+    /**
+     * Passe l'utilisateur au niveau suivant
+     * @return void
+     */
+    public function nextLevel(Level $level, User $profile): void
+    {
+        if($profile->current_xp >= $level->required_xp) {
+            $profile->current_level += 1;
+            $profile->current_xp = 0;
         }
     }
 }
